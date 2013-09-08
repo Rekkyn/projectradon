e341f1ac86505c303a3addce70aca55cda5906ae
Rekkyn <reedesler@gmail.com>
2013-09-07 20:50:05 -0700

    Added game window.

---
+++ src/radon/Game.java
@@ -0,0 +1,34 @@
+package radon;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame {
+    
+    static AppGameContainer appgc;
+    public static final String NAME = "Project Radon";
+    public static final int PLAY = 0;
+    public static int width = 800;
+    public static int height = 600;
+    
+    public Game(String name) {
+        super(name);
+    }
+    
+    public static void main(String[] args) {
+        try {
+            appgc = new AppGameContainer(new Game(NAME));
+            appgc.setDisplayMode(width, height, false);
+            appgc.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {
+        addState(new Play(PLAY));
+    }
+}
